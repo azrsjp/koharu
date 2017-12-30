15472df1643c2c5c96d14338af97cb3e8c8129b4
gomachan_7 <tt@a-azarashi.jp>
2017-12-30 13:56:55 +0900

    Implemenet flick test

--- src/State/HomeState.ts
+++ src/State/HomeState.ts
@@ -1,12 +1,20 @@
 import {CustomState} from "Core/State/CustomState";
 
 export class HomeState extends CustomState {
+    private debugText: Phaser.Text;
+    private isDown: boolean = false;
+    private static FlickThrethold: number = 70;
 
     init() {
         super.init();
 
-        let hello = this.game.add.text(0, 0, "Hello, world", { font: '14px', fill: '#fff'});
-        hello.resolution = window.devicePixelRatio;
+        this.debugText = this.game.add.text(0, 0, "Hello, world", { font: '14px', fill: '#fff'});
+        this.debugText.resolution = window.devicePixelRatio;
+
+        this.game.input.maxPointers = 1;
+        this.game.input.addMoveCallback(this.onMove, this);
+        this.game.input.onDown.add(this.onDown, this);
+        this.game.input.onUp.add(this.onUp, this);
     }
 
     preload() {
@@ -22,6 +30,39 @@ export class HomeState extends CustomState {
     adjust() {
     }
 
+    update() {
+    }
+
     // private
 
+    private onMove(pointer: Phaser.Pointer) {
+        if (!this.isDown) {
+            return;
+        }
+        const distance = Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown);
+        const isOverThretholdDistance = distance > HomeState.FlickThrethold;
+
+        if (isOverThretholdDistance && this.game.input.activePointer.isDown) {
+            this.debugText.text = "Swiped";
+        }
+    }
+
+    private onDown(pointer: Phaser.Pointer) {
+        this.isDown = true;
+        this.debugText.text = "";
+    }
+
+    private onUp(pointer: Phaser.Pointer) {
+        if (!this.isDown) {
+            return;
+        }
+        this.isDown = false;
+
+        const distance = Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown);
+        const isInThretholdDistance = distance <= HomeState.FlickThrethold;
+
+        if (isInThretholdDistance) {
+            this.debugText.text = "onUp";
+        }
+    }
 }
