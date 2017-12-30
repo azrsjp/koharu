072c73dc0e79ff614e953f35bed0b5c1c2afdc66
gomachan_7 <tt@a-azarashi.jp>
2017-12-30 15:18:25 +0900

    Implment game loop

--- src/State/HomeState.ts
+++ src/State/HomeState.ts
@@ -5,7 +5,9 @@ export class HomeState extends CustomState {
     private scoreText: Phaser.Text;
     private score: number = 0;
     private isDown: boolean = false;
-    private koharu: Koharu;
+    private koharu: Koharu = null;
+    private isCleared: boolean = false;
+    private isGameOver: boolean = false;
     private static FlickThrethold: number = 70;
 
     init() {
@@ -18,9 +20,7 @@ export class HomeState extends CustomState {
         this.game.input.addMoveCallback(this.onMove, this);
         this.game.input.onDown.add(this.onDown, this);
         this.game.input.onUp.add(this.onUp, this);
-
-        this.koharu = new Koharu(this.game);
-        this.game.add.existing(this.koharu);
+        this.generateKoharu(true);
     }
 
     preload() {
@@ -41,17 +41,60 @@ export class HomeState extends CustomState {
 
     // private
 
+    private judgeTap() {
+        if (this.isGameOver || this.isCleared) {
+            return;
+        }
+        this.isCleared = true;
+        this.incrementScore();
+    }
+
+    private judgeFlick() {
+        if (this.isGameOver || this.isCleared) {
+            return;
+        }
+        this.isCleared = true;
+        this.incrementScore();
+    }
+
+    private generateKoharu(isStart: boolean) {
+        if (this.koharu != null) {
+            this.game.world.remove(this.koharu);
+            this.koharu = null;
+        }
+        if (!isStart && (!this.isCleared || this.isGameOver)) {
+            this.isGameOver = true;
+            return;
+        }
+
+        this.isCleared = false;
+
+        this.koharu = new Koharu(this.game);
+        this.game.add.existing(this.koharu);
+        this.koharu.position.setTo(320, 200);
+        this.game.add
+            .tween(this.koharu)
+            .to({x: -this.koharu.width}, 1000, Phaser.Easing.Linear.caller)
+            .start()
+            .onComplete.addOnce(() => this.generateKoharu(false), this);
+    }
+
     private incrementScore() {
         this.score += 1;
         this.scoreText.text = "小春ちゃん: " + this.score.toString();
     }
 
     private onMove(pointer: Phaser.Pointer) {
-        if (!this.isDown) {
+        if (!this.isDown || this.isGameOver || this.isCleared) {
             return;
         }
         const distance = Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown);
         const isOverThretholdDistance = distance > HomeState.FlickThrethold;
+
+        if (isOverThretholdDistance) {
+            console.log("flicked");
+            this.judgeFlick();
+        }
     }
 
     private onDown(pointer: Phaser.Pointer) {
@@ -59,12 +102,17 @@ export class HomeState extends CustomState {
     }
 
     private onUp(pointer: Phaser.Pointer) {
-        if (!this.isDown) {
+        if (!this.isDown || this.isGameOver || this.isCleared) {
+            this.isDown = false;
             return;
         }
         this.isDown = false;
 
         const distance = Phaser.Point.distance(this.game.input.activePointer.position, this.game.input.activePointer.positionDown);
         const isInThretholdDistance = distance <= HomeState.FlickThrethold;
+
+        if (isInThretholdDistance) {
+            this.judgeTap();
+        }
     }
 }
